02d375fa7ff002e6b3fba88cfbb0746d8fc19c52
binz <xinkiang@gmail.com>
2015-03-27 20:20:25 +0800

    test

---
+++ spec/digger_spec.rb
@@ -0,0 +1,26 @@
+require 'digger'
+
+http = Digger::HTTP.new
+page = http.fetch_page('http://nan.so/')
+
+pattern = Digger::Pattern.new({type: 'css_many', value: '.sites>a>span' })
+
+class Item < Digger::Model
+  css_many sites: '.sites>a>span'
+end
+
+describe Digger do
+  it "http should fetch a page" do
+    expect(page.code).to eq(200)
+  end
+
+  it "pattern should match content" do
+    sites = pattern.match_page(page)
+    expect(sites.include?('百度网盘')).to eq(true)
+  end
+
+  it "model should dig content" do
+    item = Item.new.match_page(page)
+    expect(item[:sites].include?('读远')).to be(true)
+  end
+end
